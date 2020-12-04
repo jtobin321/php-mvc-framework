2fb686b192dd01577247c48fc04da8d391ac077f
Jack Tobin <jtobin321@gmail.com>
2020-12-04 17:16:30 -0500

    adding pages controller and updating core

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -6,4 +6,36 @@
 */
 class Core {
   protected $currentController = 'Pages';
+  protected $currentMethod = 'index';
+  protected $params = [];
+
+  public function __construct() {
+    $url = $this->getUrl();
+    $controller = '';
+    if (count($url) > 0) {
+      $controller = ucwords($url[0]);
+    }
+
+    $ctrlFile = "../app/controllers/{$controller}.php";
+    if (file_exists($ctrlFile)) {
+      $this->currentController = $controller;
+      unset($url[0]);
+    } 
+
+    $ctrlFile = "../app/controllers/{$this->currentController}.php";
+    require_once $ctrlFile;
+
+    $this->currentController = new $this->currentController();
+  }
+
+  public function getUrl() {
+    $url = [];
+    if (isset($_GET['url'])) {
+      $url = rtrim($_GET['url'], '/');
+      $url = filter_var($url, FILTER_SANITIZE_URL);
+      $url = explode('/', $url);
+    }
+
+    return $url;
+  }
 }
